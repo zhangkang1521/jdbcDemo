0bc5bc1edae2a0e9a849111cb85873bf2203a5e8
zhangkang <zhangkang@lvmama.com>
2017-01-09 15:12:30 +0800

    log4jdbc 记录完整sql,时间

--- src/main/java/org/zk/jdbc/JdbcDemo.java
+++ src/main/java/org/zk/jdbc/JdbcDemo.java
@@ -1,23 +1,26 @@
 package org.zk.jdbc;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.sql.*;
 
 /**
  * Created by zhangkang on 2017/1/4.
  */
 public class JdbcDemo {
+    private static Logger logger = LoggerFactory.getLogger(JdbcDemo.class);
+
     public static void main(String[] args) throws Exception {
-        Class.forName("com.mysql.jdbc.Driver");
-        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/zk", "root", "123456");
-        Statement stmt = conn.createStatement();
-        ResultSet rs = stmt.executeQuery("select * from tb_user");
+        Class.forName("net.sf.log4jdbc.DriverSpy");
+        Connection conn = DriverManager.getConnection("jdbc:log4jdbc:mysql://localhost:3306/zk", "root", "123456");
+        PreparedStatement stmt = conn.prepareStatement("select * from tb_user where id=?");
+        stmt.setInt(1, 3);
+        ResultSet rs = stmt.executeQuery();
         while (rs.next()) {
-            System.out.print(rs.getInt("id") + ", ");
-            System.out.print(rs.getString("username"));
-            System.out.println();
+//            System.out.print(rs.getInt("id") + ", ");
+//            System.out.print(rs.getString("username"));
+//            System.out.println();
         }
         rs.close();
         stmt.close();
